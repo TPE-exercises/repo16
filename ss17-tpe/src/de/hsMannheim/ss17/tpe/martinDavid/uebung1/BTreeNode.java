7218f4881f62648172b56b4ce3981d50c373bcf5
David Nadoba <dnadoba@gmail.com>
2017-03-27 15:15:16 +0200

    add bestInsertPositionToLeftByBinarySearch to BTreeNode

--- ss17-tpe/src/de/hsMannheim/ss17/tpe/martinDavid/uebung1/BTreeNode.java
+++ ss17-tpe/src/de/hsMannheim/ss17/tpe/martinDavid/uebung1/BTreeNode.java
@@ -17,4 +17,44 @@ public class BTreeNode {
 	boolean contains(Integer o) {
 		return false;
 	}
+	
+	/**
+	 * finds the best insert position for an element in an ascending ordered
+	 * subrange from 0 to `endIndex`
+	 * 
+	 * @param array
+	 *            - array to find the inserting position
+	 * @param endIndex
+	 *            - last index of the ascending ordered subrange
+	 * @param elementToInsert
+	 *            - element to insert
+	 * @return the index of the best insert position for the `elementToInsert`
+	 */
+	private static int bestInsertPositionToLeftByBinarySearch(Integer[] array, int endIndex, Integer elementToInsert) {
+
+		int startIndex = 0;
+
+		while (startIndex < endIndex) {
+			int distanceBetweenStartAndEndIndex = endIndex - startIndex;
+			int middleIndex = startIndex + (distanceBetweenStartAndEndIndex / 2);
+
+			int middleElement = array[middleIndex];
+
+			int rightIndex = middleIndex + 1;
+
+			int rightElement = array[rightIndex];
+
+			boolean isBestInsertPosition = middleElement <= elementToInsert && 
+											rightElement >= elementToInsert;
+
+			if (isBestInsertPosition) {
+				return rightIndex;
+			} else if (middleElement > elementToInsert) {
+				endIndex = middleIndex;
+			} else {
+				startIndex = middleIndex;
+			}
+		}
+		return endIndex;
+	}
 }
