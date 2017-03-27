4b55b43be782160ca9d8e2eba3dc5c1177d63224
martin <schalter.dev@gmail.com>
2017-03-27 15:32:16 +0200

    finished insert method

--- ss17-tpe/src/de/hsMannheim/ss17/tpe/martinDavid/uebung1/BTreeNode.java
+++ ss17-tpe/src/de/hsMannheim/ss17/tpe/martinDavid/uebung1/BTreeNode.java
@@ -5,7 +5,7 @@ public class BTreeNode {
 	private Integer[] elements;
 	private BTreeNode[] children;
 	
-	public BTreeNode(int ordinal) {
+	BTreeNode(int ordinal) {
 		// We need one element and children more that we can check if this node have to be splitted
 		elements = new Integer[2 * ordinal + 1];
 		children = new BTreeNode[2 * ordinal + 1 + 1];
@@ -25,9 +25,25 @@ public class BTreeNode {
 		
 		if(!hasChildren()) {
 			//insert the element into this node
+			int indexToInsert = bestInsertPositionToLeftByBinarySearch(elements, elements.length, element);
+			
+			if(indexToInsert == -1) {
+				//element is already in the node
+				return false;
+			} else {
+				insertIntoNode(element, indexToInsert);
+				return true;
+			}
 		}
 		
-		return false;
+		//this node is not a leaf -> search the node to insert this element
+		int indexToInsert = bestInsertPositionToLeftByBinarySearch(elements, elements.length, element);
+		
+		if(indexToInsert == -1) {
+			return false;
+		} else {
+			return children[indexToInsert].insert(element);
+		}
 	}
 	
 	int size() {
