f15e2daddec12c4a7eb6e2565f414e843403b9ce
Martin Schalter <schalter.dev@gmail.com>
2017-04-03 16:21:25 +0200

    added test for inserting same elements

--- ss17-tpe/src/de/hsMannheim/ss17/tpe/martinDavid/uebung1/MyBTreeTest.java
+++ ss17-tpe/src/de/hsMannheim/ss17/tpe/martinDavid/uebung1/MyBTreeTest.java
@@ -31,6 +31,26 @@ public class MyBTreeTest {
 		assertEquals(elements.length, tree.size());
 	}
 	@Test
+	public void insertSameElementsTest() {
+		Integer[] elements = {5, 1, 9, 15, 18, 20, 5, 22} ;
+		MyBTree tree = new MyBTree(1);
+		for(int i = 0; i < elements.length; i++) {
+			Integer element = elements[i];
+			
+			boolean didInsertElement = tree.insert(element);
+			if(i != 6) { //6 is the index of the second 5
+				assertTrue("should insert element " + element.toString(), didInsertElement);
+			} else {
+				assertFalse("sould not insert element " + element.toString(), didInsertElement);
+			}
+			
+			boolean treeContainsElement = tree.contains(element);
+			assertTrue("should contain element " + element.toString(), treeContainsElement);
+			tree.printInorder();
+		}
+		assertEquals(elements.length - 1, tree.size());
+	}
+	@Test
 	public void containTest() {
 		Integer[] elements = {5, 1, 9};
 		MyBTree tree = new MyBTree(1);
