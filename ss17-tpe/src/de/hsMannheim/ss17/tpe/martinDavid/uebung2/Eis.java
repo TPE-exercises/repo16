f4ff13d3603b1a713e8f80fbdb87b0a54746c683
David Nadoba <dnadoba@gmail.com>
2017-04-21 10:18:42 +0200

    Create package for exercise 2 and add class Eis with all required methods, properties and documentation but without an implementation

---
+++ ss17-tpe/src/de/hsMannheim/ss17/tpe/martinDavid/uebung2/Eis.java
@@ -0,0 +1,50 @@
+package de.hsMannheim.ss17.tpe.martinDavid.uebung2;
+
+public class Eis {
+	/**
+	 * Bezeichnung des Eises
+	 */
+	private String name;
+	/**
+	 * Der Preis in Euro
+	 */
+	private String preis;
+	/**
+	 * Typ des Behältnisses 
+	 */
+	private String behaeltnis;
+	
+	/**
+	 * Art des Eises (Bällchen, Bälle, Spaghettis, usw)
+	 */
+	private String art;
+	
+	/**
+	 * Alle Sorten die das Eis beinhaltet (Vanille, Schokolade, usw)
+	 */
+	private String[] sorten;
+	
+	/**
+	 * Alle Besonderheiten die zusätzlich zu den Sorten dem Eis beigefügt werden (Sahne, Schockstäusel, Kekse, usw)
+	 */
+	private String[] extras;
+	
+	/**
+	 * Bereitet alles vor um mit dem befüllen zu beginnen
+	 */
+	void vorbereiten(){
+		
+	}
+	/**
+	 * Behältnis mit Sorten nach Art fu ̈llen
+	 */
+	void fuellen(){
+		
+	}
+	/**
+	 * Eis mit Extras dekorieren
+	 */
+	void dekorieren(){
+		
+	}
+}
