7e7a5ff0498483ee5fdb9bb6a2ee548576dd5393
Martin Schalter <schalter.dev@gmail.com>
2017-04-29 14:13:34 +0200

    Addedd test for a null String

--- ss17-tpe/src/de/hsMannheim/ss17/tpe/martinDavid/uebung2/CrypterCaesarTest.java
+++ ss17-tpe/src/de/hsMannheim/ss17/tpe/martinDavid/uebung2/CrypterCaesarTest.java
@@ -8,21 +8,23 @@ public class CrypterCaesarTest {
 
 	@Test
 	public void encryptCaesarTest() {
-		CrypterCaesar caesar = new CrypterCaesar(0);
+		Crypter caesar = new CrypterCaesar(0);
 		assertEquals("AAA", caesar.encrypt("aaa"));
 		
 		caesar = new CrypterCaesar(28); //key = 2
 		assertEquals("CDB", caesar.encrypt("ABZ"));
 		assertEquals("az&r", caesar.decrypt(caesar.encrypt("az&r")));
+		assertEquals(null, caesar.encrypt(null));
 	}
 	
 	@Test
 	public void decryptCaesarTest() {
-		CrypterCaesar caesar = new CrypterCaesar(0);
+		Crypter caesar = new CrypterCaesar(0);
 		assertEquals("aaa", caesar.decrypt("aAa"));
 		
 		caesar = new CrypterCaesar(28); //key = 2
 		assertEquals("abz", caesar.decrypt("CDB"));
+		assertEquals(null, caesar.decrypt(null));
 	}
 	
 }
