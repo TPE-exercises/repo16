2396da979dafc86be59b1a416daffa9a2bd2195e
Martin Schalter <schalter.dev@gmail.com>
2017-05-05 12:29:48 +0200

    OOT testclass

---
+++ ss17-tpe/src/de/hsMannheim/ss17/oot/martinDavid/uebungBlackBox/PosEingabeTest.java
@@ -0,0 +1,14 @@
+package de.hsMannheim.ss17.oot.martinDavid.uebungBlackBox;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PosEingabeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
