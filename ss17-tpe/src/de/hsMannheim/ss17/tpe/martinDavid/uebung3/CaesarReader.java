c363be3581851545ae46dd9439ed5c44ae83ca10
Martin Schalter <schalter.dev@gmail.com>
2017-05-08 12:08:54 +0200

    Created classes for Aufgabe 2

---
+++ ss17-tpe/src/de/hsMannheim/ss17/tpe/martinDavid/uebung3/CaesarReader.java
@@ -0,0 +1,13 @@
+package de.hsMannheim.ss17.tpe.martinDavid.uebung3;
+
+import java.io.FilterReader;
+import java.io.Reader;
+
+public class CaesarReader extends FilterReader {
+
+	protected CaesarReader(Reader in) {
+		super(in);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ ss17-tpe/src/de/hsMannheim/ss17/tpe/martinDavid/uebung3/CaesarWriter.java
@@ -0,0 +1,13 @@
+package de.hsMannheim.ss17.tpe.martinDavid.uebung3;
+
+import java.io.FilterWriter;
+import java.io.Writer;
+
+public class CaesarWriter extends FilterWriter {
+
+	protected CaesarWriter(Writer out) {
+		super(out);
+		// TODO Auto-generated constructor stub
+	}
+
+}
