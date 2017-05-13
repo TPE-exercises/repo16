86b7afbc2a8be59f4cdbb6002ca7b81f1ee84948
David Nadoba <dnadoba@gmail.com>
2017-05-13 12:07:10 +0200

    Add LinkedListStackTest which extends StackTest

---
+++ ss17-tpe/src/de/hsMannheim/ss17/tpe/martinDavid/utilitiies/LinkedListStackTest.java
@@ -0,0 +1,10 @@
+package de.hsMannheim.ss17.tpe.martinDavid.utilitiies;
+
+public class LinkedListStackTest extends StackTest {
+
+	@Override
+	public void setup() {
+		stack = new LinkedListStack(initalMaxSize);
+	}
+
+}
