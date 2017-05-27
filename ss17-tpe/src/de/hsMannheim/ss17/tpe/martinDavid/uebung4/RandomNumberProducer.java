0fa3a2a18ee84047f5e0e4ddb82759aab6b77da7
David Nadoba <dnadoba@gmail.com>
2017-05-28 00:48:35 +0200

    First untested implementation of RandomNumberGenerator class

---
+++ ss17-tpe/src/de/hsMannheim/ss17/tpe/martinDavid/uebung4/RandomNumberProducer.java
@@ -0,0 +1,38 @@
+package de.hsMannheim.ss17.tpe.martinDavid.uebung4;
+
+import java.util.Random;
+
+public class RandomNumberProducer extends Thread {
+	private Random generator;
+	private long delay;
+	private ThreadSafeRingBuffer ringBuffer;
+	/**
+	 * creates a thread which will produce a random number, 
+	 * put it in the ring buffer and waits a given duration and 
+	 * repeats it until it is interrupted
+	 * @param ringBuffer ring buffer to put the numbers in
+	 * @param seed random number generator seed
+	 * @param delay wait duration
+	 */
+	public RandomNumberProducer(ThreadSafeRingBuffer ringBuffer, long seed, long delay) {
+		this.ringBuffer = ringBuffer;
+		this.generator = new Random(seed);
+		this.delay = delay;
+	}
+	@Override
+	public void run() {
+		while(!isInterrupted()) {
+			
+			int randomInt = generator.nextInt();
+			Integer randomInteger = new Integer(randomInt);
+			
+			try {
+				ringBuffer.put(randomInteger);
+				sleep(delay);
+			} catch (InterruptedException e) {
+				System.out.println("RandomNumberProducer interupted");
+				interrupt();
+			}
+		}
+	}
+}
