d3d88440e894881c1e2600f8fc82ddd75fde27d6
David Nadoba <dnadoba@gmail.com>
2017-05-28 01:47:14 +0200

    First untested implementation of InterruptTimer class

---
+++ ss17-tpe/src/de/hsMannheim/ss17/tpe/martinDavid/utilitiies/queue/InterruptTimer.java
@@ -0,0 +1,34 @@
+package de.hsMannheim.ss17.tpe.martinDavid.utilitiies.queue;
+
+public class InterruptTimer extends Thread {
+	private Thread[] threads;
+	private long timeout;
+	/**
+	 * Interrupts all threads after a given timeout and waits until all threads die
+	 * @param threads threads to interrupt
+	 * @param timeout timeout in milliseconds
+	 */
+	public InterruptTimer(Thread[] threads, long timeout) {
+		this.threads = threads;
+		this.timeout = timeout;
+	}
+	@Override
+	public void run() {
+		try {
+			sleep(timeout);
+		} catch (InterruptedException e) {
+			//it's okay
+		}
+		for(Thread thread: threads) {
+			thread.interrupt();
+		}
+		for(Thread thread: threads) {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				break;
+			}
+		}
+	}
+
+}
