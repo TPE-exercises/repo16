6795ead91ed4e7c2092961eafbf0ef97c60914f1
SchalterDev <schalter.dev@gmail.com>
2017-07-05 10:57:38 +0200

    Aufgabe 6

--- ss17-tpe/src/klausurvorbereitung/ss07/aufgabe5/Queuee.java
+++ ss17-tpe/src/klausurvorbereitung/ss07/aufgabe5/Queuee.java
@@ -1,12 +1,24 @@
 package klausurvorbereitung.ss07.aufgabe5;
 
-public class Queuee<E> {
+import java.util.Iterator;
+
+public class Queuee<E> implements Iterable<E> {
 	
 	public void put(E object) {
 		
 	}
 	
 	public E leave() {
+		return null;
+	}
+	
+	public boolean isEmpty() {
+		return true;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
 		return null;
 	}
 	
@@ -17,4 +29,24 @@ class Main {
 		Queuee<Integer> que1 = new Queuee<>();
 		Queuee<String> que2 = new Queuee<>();
 	}
+}
+
+class MyIterator<E> implements Iterator<E> {
+
+	private Queuee<E> queue;
+	
+	public MyIterator(Queuee<E> queuee) {
+		queue = queuee;
+	}
+	
+	@Override
+	public boolean hasNext() {
+		return !queue.isEmpty();
+	}
+
+	@Override
+	public E next() {
+		return queue.leave();
+	}
+	
 }
